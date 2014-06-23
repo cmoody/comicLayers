b5113b036fd96c6a6c8f9587d26012fbcba358d6
Chase Moody <chase.moody@mutualmobile.com>
2014-06-23 13:25:55 -0500

    Added gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,17 +5,26 @@ module.exports = function(grunt) {
     connect: {
       server: {
         options: {
-          port: 8000,
+          port: 8080,
           base: 'src',
           keepalive: true
         }
       }
+    },
+
+    'gh-pages': {
+      options: {
+        base: 'src'
+      },
+      src: ['**/*']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-gh-pages');
 
   // Default task(s).
   grunt.registerTask('default', ['connect']);
+  grunt.registerTask('deploy', ['gh-pages']);
 
 };
